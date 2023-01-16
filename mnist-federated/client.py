afd2e3521f67bc5eb5c3bf7d5686eb03fa3ee9a9
dimalkavindu <dimalkavindu@gmail.com>
2023-01-16 20:55:44 +0530

    Adding mnist federated version

--- mnist-federated/client.py
+++ mnist-federated/client.py
@@ -3,23 +3,31 @@ import os
 import flwr as fl
 import tensorflow as tf
 
-
 # Make TensorFlow log less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-# Load model and data (MobileNetV2, CIFAR-10)
-model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
 model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 # Define Flower client
-class CifarClient(fl.client.NumPyClient):
+class MnistClient(fl.client.NumPyClient):
     def get_parameters(self, config):
         return model.get_weights()
 
     def fit(self, parameters, config):
         model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32)
+        model.fit(x_train, y_train, epochs=1)
         return model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
@@ -29,4 +37,5 @@ class CifarClient(fl.client.NumPyClient):
 
 
 # Start Flower client
-fl.client.start_numpy_client(server_address="10.182.0.6:8080", client=CifarClient())
+fl.client.start_numpy_client(server_address="10.182.0.6:8080", client=MnistClient())
+
